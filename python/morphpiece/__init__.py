d2cceede8af1c5621ab88ed263801b45f4622e61
iam-kevin <kevin.al.james@gmail.com>
2020-11-23 04:41:02 +0300

    style: added objects to morphpiece/__init__.py

--- python/morphpiece/__init__.py
+++ python/morphpiece/__init__.py
@@ -1,1 +1,7 @@
 from ._version import VERSION as __version__
+
+from .processor import AlphabetProcessor
+from .config import MPConfig
+
+from .core import MorphPiece
+from .trainer import MorphPieceTrainer
