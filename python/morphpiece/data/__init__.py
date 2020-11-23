87a6ccd2cdd40655be000cec9c3533926558a545
iam-kevin <kevin.al.james@gmail.com>
2020-11-23 03:49:43 +0300

    refactor: included calls to data/__init__.py

--- python/morphpiece/data/__init__.py
+++ python/morphpiece/data/__init__.py
@@ -1,1 +1,10 @@
 from .reference_vocabulary import LowercaseSwahiliReferenceVocabulary
+
+# adding Text Transformers
+from .corpus import DataTextTransformer, TokenTransformer
+
+# adding readers
+from .corpus import LazyDataTextReader
+
+# adding Dictionary-based components
+from .corpus import WordList
